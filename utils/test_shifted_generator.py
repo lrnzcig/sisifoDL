4ffa9d0122111c77a9b8f0beac15f6ae00eee3a6
Lorenzo Rubio <lrnzcig@gmail.com>
2019-10-27 17:40:24 +0100

    improved test case

--- utils/test_shifted_generator.py
+++ utils/test_shifted_generator.py
@@ -34,6 +34,35 @@ class test_shifted(unittest.TestCase):
                           rebalance_data=False,
                           debug=False)
 
+        train_names_orig = \
+            np.array(['p_17-01-19', 'p_17-01-20', 'p_17-01-21', 'p_17-01-22', 'p_17-01-23', 'p_17-01-24', 'p_17-01-25',
+                      'p_17-01-26', 'p_17-01-27', 'p_17-01-28', 'p_17-01-29', 'p_17-01-30', 'p_17-01-31', 'p_17-02-01',
+                      'p_17-02-02', 'p_17-02-03', 'p_17-02-04', 'p_17-02-05', 'p_17-02-06', 'p_17-02-07', 'p_17-02-08',
+                      'p_17-02-09', 'p_17-02-10'])
+        train_names = [n + "_" + str(i) for n in train_names_orig for i in range(0, 15)]
+
+        self.train_gen = \
+            DataGenerator(self.dataset_reduced_std, train_names,
+                                  "spo2", batch_size=batch_size,
+                                  number_of_predictions=number_of_predictions,
+                                  window_size=window_size,
+                                  step_prediction_dates=1, shuffle=False,
+                                  rebalance_data=True, rebalance_threshold=0.5,
+                                  debug=False)
+
+        val_names_orig = \
+            np.array(['p_17-02-11', 'p_17-02-12', 'p_17-02-13', 'p_17-02-14',
+                      'p_17-02-15', 'p_17-02-16', 'p_17-02-17', 'p_17-02-18'])
+        val_names = [n + "_" + str(i) for n in val_names_orig for i in range(0, 15)]
+
+        self.val_gen = DataGenerator(self.dataset_reduced_std, val_names,
+                                "spo2", batch_size=batch_size,
+                                number_of_predictions=number_of_predictions,
+                                window_size=window_size,
+                                step_prediction_dates=1, shuffle=False,
+                                rebalance_data=True, rebalance_threshold=0.5,
+                                debug=False)
+
     def test_basic(self):
         with open("test_gen.pkl", 'wb') as output:
             pickle.dump(self.test_gen, output, pickle.HIGHEST_PROTOCOL)
@@ -42,3 +71,19 @@ class test_shifted(unittest.TestCase):
 
         self.assertEqual(np.isclose(X.sum(), 2238170.909015533), True)
         self.assertEqual(np.isclose(y.sum(), 517260.953926506), True)
+
+        with open("val_gen.pkl", 'wb') as output:
+            pickle.dump(self.val_gen, output, pickle.HIGHEST_PROTOCOL)
+
+        X, y = self.val_gen.get_all_batches()
+
+        self.assertEqual(np.isclose(X.sum(), 2267565.531491879), True)
+        self.assertEqual(np.isclose(y.sum(), 516429.0304805267), True)
+
+        with open("train_gen.pkl", 'wb') as output:
+            pickle.dump(self.train_gen, output, pickle.HIGHEST_PROTOCOL)
+
+        X, y = self.train_gen.get_all_batches()
+
+        self.assertEqual(np.isclose(X.sum(), 6315931.116053891), True)
+        self.assertEqual(np.isclose(y.sum(), 1438494.9115140745), True)
