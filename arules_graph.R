56c19ee074b053d50b162624ce1dd049b897ba7a
Lorenzo Rubio <lrnzcig@gmail.com>
2019-11-17 13:58:30 +0100

    temporary commit

--- arules_graph.R
+++ arules_graph.R
@@ -1,7 +1,52 @@
 # https://archive.ics.uci.edu/ml/datasets/Anonymous+Microsoft+Web+Data
 # https://archive.ics.uci.edu/ml/machine-learning-databases/anonymous/
 
-msweb.orig <- read.csv("~/Downloads/clickstream/anonymous-msweb.data",
+msweb.orig <- read.csv("~/Downloads/anonymous-msweb.data",
                        header=FALSE,
                        sep=",",
                        col.names=c("V1", "V2", "V3", "V4", "V5", "V6"))
+
+attribute.lines <- msweb.orig[msweb.orig$V1 == "A", c("V2", "V4", "V5")]
+colnames(attribute.lines) <- c("id", "title", "url")
+head(attribute.lines)
+
+casevote.lines <- msweb.orig[msweb.orig$V1 == "C" | msweb.orig$V1 == "V", c("V1", "V2")]
+head(casevote.lines)
+
+casevote.lines$rownames <- as.numeric(rownames(casevote.lines)) # add indexes as a new column
+dt <- data.table(casevote.lines[, !(names(casevote.lines)
+                                    %in% "parent")]) # convert to dataframe, excluding the parent column from a previos trial
+head(dt)
+
+dt[V1 == "C", parent:=rownames, by=rownames] # parents of C lines are themselves
+difference <- 1
+while (sum(is.na(dt$parent)) > 0) {
+  dt[, shiftV1 := shift(V1, difference)] # shift by "difference"
+  dt[shiftV1 == "C" & is.na(parent),
+     parent:=(rownames-difference), by=rownames] # set parent value of visits with n. of pages == "difference"
+  difference <- difference + 1
+}
+casevote.lines$parent = dt$parent
+head(casevote.lines)
+
+
+clicks <- merge(x=casevote.lines[casevote.lines$V1 == "C", c("V2", "parent")],
+                y=casevote.lines[casevote.lines$V1 == "V", c("V2", "parent")],
+                by="parent")
+colnames(clicks) <- c("remove", "userId", "attributeId")
+head(clicks)
+
+
+msweb <- merge(x=clicks[,c("userId", "attributeId")],
+               y=attribute.lines,
+               by.x=c("attributeId"),
+               by.y=c("id"))
+msweb <- msweb[order(msweb$userId),]
+head(msweb)
+
+
+msweb.items <- data.table(msweb[, c("title", "userId")])
+msweb.items[, l:=.(list(unique(title))), by=userId] # creates list of pages per user, see note
+msweb.items <- msweb.items[! duplicated(userId), l] # removes duplicated lines per user and selects only the list 
+
+head(msweb.items, 3)
